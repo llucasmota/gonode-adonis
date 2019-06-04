e8c703ed7e3cf5bea099403ed48c153f3201ee6b
llucasmota <lucas.o.mota@gmail.com>
2019-06-03 21:56:13 -0300

    alterações no ProjectController e criação de rota do project

--- start/routes.js
+++ start/routes.js
@@ -6,5 +6,14 @@ Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
-Route.post('files', 'FileController.store')
+// Route.post('files', 'FileController.store')
 Route.get('/files/:id', 'FileController.show')
+
+Route.group(() => {
+  Route.post('files', 'FileController.store')
+  Route.resource('projects', 'ProjectController').apiOnly()
+})
+// Route.get('project', 'ProjectController.index')
+/**
+ * Disponibilizar todas as rotas numa única linha
+ */
