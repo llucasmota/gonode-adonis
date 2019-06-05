97f2474834a16623fd25bf0373ad23c0a37cf443
llucasmota <lucas.o.mota@gmail.com>
2019-06-05 07:20:15 -0300

    Ajustando relacionamentos de Project e Task

--- app/Models/Task.js
+++ app/Models/Task.js
@@ -4,6 +4,15 @@
 const Model = use('Model')
 
 class Task extends Model {
+  project () {
+    return this.belongsTo('App/Model/Project')
+  }
+  user () {
+    return this.belongsTo('App/Model/User')
+  }
+  file () {
+    return this.belongsTo('App/Model/File')
+  }
 }
 
 module.exports = Task
