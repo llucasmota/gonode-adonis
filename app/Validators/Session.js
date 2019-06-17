4cd43fc69b3197b74dc2347dffec87313d8c08f2
llucasmota <lucas.o.mota@gmail.com>
2019-06-17 08:16:26 -0300

    Os arquivos de validation foram alterados para trabalharem com internacionalização nas mensagens de retorno e para isso foi adicionada uma nova dependencia do Adonis(Antl), bem como foi adicionado arquivo com as mensagens

--- app/Validators/Session.js
+++ app/Validators/Session.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Antl = use('Antl')
+
 class Session {
   get validateAll () {
     return true
@@ -10,6 +12,9 @@ class Session {
       email: 'required|email', // obrigatório e precisa estar no formato de email
       password: 'required'
     }
+  }
+  get messages () {
+    return Antl.list('validation')
   }
 }
 
