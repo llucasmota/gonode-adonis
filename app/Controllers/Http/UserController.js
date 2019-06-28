8fb2c51a74d0029f66c5b566d7497f91e72fd7cb
llucasmota <lucas.o.mota@gmail.com>
2019-06-27 23:33:24 -0300

    -Adicionada codificação no controller de usuário para utilizar Transaction na persistencia de dados que possuem dependencias com UserAdress

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,12 +1,16 @@
 'use strict'
+const DataBase = use('Database')
 const User = use('App/Models/User')
 class UserController {
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
     const adresses = request.input('adresses')
+    const trx = await DataBase.beginTransaction()
 
-    const user = await User.create(data)
-    await user.adresses().createMany(adresses)
+    const user = await User.create(data, trx)
+    await user.adresses().createMany(adresses, trx)
+
+    await trx.commit()
 
     return user
   }
